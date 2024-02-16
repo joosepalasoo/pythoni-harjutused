7d1ec7d006baca3dd681f8d2fdc5fea0a47ec9e0
joosepalasoo <144770299+joosepalasoo@users.noreply.github.com>
2024-02-16 09:22:42 +0200

    update
    tkinter

--- python3gui/tkinter02.py
+++ python3gui/tkinter02.py
@@ -1,6 +1,6 @@
 #joosep alasoo
 #tkinter yl02
-#06.02.2024
+#16.02.2024
 from tkinter import *
 
 #akna seaded
@@ -9,11 +9,48 @@ aken.title("kalkulaator")
 aken.geometry("200x200")
 
 #tekst
-Text=Label(aken, text="siia kunagi vastus!", font="tahoma 12").pack()
-
+text=Label(aken, text="siia kunagi vastus!", font="tahoma 12")
+text.grid(row=0, column=0, columnspan=4, padx=2, pady=2)
 #nupud
-nupp1=Button(aken, text="1")
-nupp1.grid(row=0, column=1)
+
+nupp1 = Button(aken, text="7", font=("tahoma", 12) , width=4)
+nupp1.grid(row=1, column=0, padx=2, pady=2)
+nupp2 = Button(aken, text="8", font=("tahoma", 12) , width=4)
+nupp2.grid(row=1, column=1, padx=2, pady=2)
+nupp3 = Button(aken, text="9", font=("tahoma", 12) , width=4)
+nupp3.grid(row=1, column=2, padx=2, pady=2)
+nupp4 = Button(aken, text="/", font=("tahoma", 12) , width=4)
+nupp4.grid(row=1, column=3, padx=2, pady=2)
+
+nupp1 = Button(aken, text="4", font=("tahoma", 12) , width=4)
+nupp1.grid(row=2, column=0, padx=2, pady=2)
+nupp2 = Button(aken, text="5", font=("tahoma", 12) , width=4)
+nupp2.grid(row=2, column=1, padx=2, pady=2)
+nupp3 = Button(aken, text="6", font=("tahoma", 12) , width=4)
+nupp3.grid(row=2, column=2, padx=2, pady=2)
+nupp4 = Button(aken, text="*", font=("tahoma", 12) , width=4)
+nupp4.grid(row=2, column=3, padx=2, pady=2)
+
+nupp1 = Button(aken, text="1", font=("tahoma", 12) , width=4)
+nupp1.grid(row=3, column=0, padx=2, pady=2)
+nupp2 = Button(aken, text="2", font=("tahoma", 12) , width=4)
+nupp2.grid(row=3, column=1, padx=2, pady=2)
+nupp3 = Button(aken, text="3", font=("tahoma", 12) , width=4)
+nupp3.grid(row=3, column=2, padx=2, pady=2)
+nupp4 = Button(aken, text="-", font=("tahoma", 12) , width=4)
+nupp4.grid(row=3, column=3, padx=2, pady=2)
+
+nupp1 = Button(aken, text="0", font=("tahoma", 12) , width=4)
+nupp1.grid(row=4, column=0, padx=2, pady=2)
+nupp2 = Button(aken, text=",", font=("tahoma", 12) , width=4)
+nupp2.grid(row=4, column=1, padx=2, pady=2)
+nupp3 = Button(aken, text="=", font=("tahoma", 12) , width=4)
+nupp3.grid(row=4, column=2, padx=2, pady=2)
+nupp4 = Button(aken, text="+", font=("tahoma", 12) , width=4)
+nupp4.grid(row=4, column=3, padx=2, pady=2)
+
+
+
 
 
 aken.mainloop()
